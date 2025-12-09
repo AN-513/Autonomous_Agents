8d7f6e1bf0766f3e7f47c1a13365bda342e73896
AN-513 <afonsonoia@hotmail.com>
2025-12-09 12:57:10 +0000

    fix: variable names

--- Drafts/demo_random_lighthouse.py
+++ Drafts/demo_random_lighthouse.py
@@ -1,17 +1,16 @@
-from Classes import agent, stats
+from Classes import stats, agent
 from Envoirments import env_lighthouse
 
 if __name__ == "__main__":
-    from Classes import agent
-    agent = agent.Agent()
+    _agent = agent.Agent()
     clusterStats = stats.StatsCluster()
     for _ in range(10):
-        stats = stats.Stats()
-        env = env_lighthouse.Light_House(agent=agent, stats=stats, light_reach=100, width=5, height=5, num_walls=5)
+        _stats = stats.Stats()
+        env = env_lighthouse.Light_House(agent=_agent, stats=_stats, light_reach=100, width=5, height=5, num_walls=5)
         env.display_gui()
-        stats.print()
+        _stats.print()
         print()
-        clusterStats.add_stats(stats)
+        clusterStats.add_stats(_stats)
 
     clusterStats.display_plots()
     clusterStats.display_heatmap()
